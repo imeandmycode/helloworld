c54b617328c8872c8e8b7d41885425e4a4eca052
imeandmycode <imeandmycode@protonmail.com>
2021-08-15 17:56:14 +0530

    helloworld python version completed

---
+++ python/README.md
@@ -0,0 +1,3 @@
+Hello World Project
+Python Version
+Creator - I Me and My Code
---
+++ python/main.py
@@ -0,0 +1,11 @@
+"""
+Hello World Project
+Python Version
+Creator - I Me and My Code
+"""
+
+
+def helloworld():
+    print("Hello, World!")
+
+helloworld()
