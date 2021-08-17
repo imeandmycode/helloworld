609a33c507a96436df081bfabfb4aa892f29cf4e
imeandmycode <imeandmycode@protonmail.com>
2021-08-17 17:47:05 +0530

    C Version Created

---
+++ c/README.md
@@ -0,0 +1,3 @@
+Hello World Project
+C Version
+Creator - I Me and My Code
---
+++ c/main.c
@@ -0,0 +1,11 @@
+// Hello World Project
+// C Version
+// Creator - I Me and My Code
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+
+    return 0;
+}
